cba260ea3b1888f9be00bc608f92aaa70a053b2d
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-07-28 00:43:17 +0400

    style: add footer header and fetch data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+
+import { Container } from "react-bootstrap";
+
 const App = () => {
-  return <h1>Welcome to ProShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomePage />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
