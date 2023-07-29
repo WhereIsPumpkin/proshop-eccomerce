da362aafdb2e0a7cdc7b34d13be9e28d444e94cd
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-07-29 17:06:54 +0400

    feat: add controller folder + express routes

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import porductRoutes from "./routes/productRoutes.js";
 dotenv.config();
 import connect from "./database/mongo.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 connect();
@@ -16,13 +17,9 @@ app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", porductRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
